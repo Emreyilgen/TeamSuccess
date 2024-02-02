9b0ee32c5152023508509fafaf1deab78fad0015
Agucak8086 <ncysuleuck@gmail.com>
2024-02-02 18:05:50 +0300

    WishListPage

--- src/test/java/allover_project/pages/customerPages/SearchPage.java
+++ src/test/java/allover_project/pages/customerPages/SearchPage.java
@@ -1,9 +1,13 @@
 package allover_project.pages.customerPages;
 
+import allover_project.utilities.Driver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class SearchPage {
+
+    public SearchPage(){PageFactory.initElements(Driver.getDriver(),this);}
 
 
     @FindBy(className = "register inline-type")
