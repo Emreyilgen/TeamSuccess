363e2f36d8e60d767d16d505e9201b17505ac3d8
Agucak8086 <ncysuleuck@gmail.com>
2024-02-02 19:00:14 +0300

    Day2

--- src/test/java/allover_project/pages/customerPages/WishListPage.java
+++ src/test/java/allover_project/pages/customerPages/WishListPage.java
@@ -1,5 +1,32 @@
 package allover_project.pages.customerPages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class WishListPage {
+
+    @FindBy(className = "register inline-type")
+public WebElement registerIcon;//Gökhan register ıcon on homepage
+
+    @FindBy(id = "reg_username")
+    public WebElement username;//Gökhan username of Sign Up part
+
+    @FindBy(id = "reg_email")
+    public WebElement email;//Gökhan email of Sign Up part
+
+    @FindBy(id = "reg_password")
+    public WebElement password;//Gökhan password of Sign Up part
+
+    @FindBy(id = "register-policy")
+    public WebElement privacyPolicyBox;//Gökhan box of Privacy Policy part
+
+    @FindBy(className = "register_as_vendor")
+    public WebElement signUpAsAVendor;//Gökhan Sign Up As A Vendor part
+
+    @FindBy(name = "register")
+    public WebElement signUpButton;//Gökhan Sign Up Button part
+
+    @FindBy()
+    public WebElement becomeAVendor;//Gökhan Become A Vendor part
 
 }
